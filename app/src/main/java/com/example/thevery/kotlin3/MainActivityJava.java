55654876df2bff7bac14a6acc16c501722e8fa9f
Ildar Karimov <thevery@yandex-team.ru>
2017-05-28 22:49:10 +0300

    Added retrolambda plugin

--- app/src/main/java/com/example/thevery/kotlin3/MainActivityJava.java
+++ app/src/main/java/com/example/thevery/kotlin3/MainActivityJava.java
@@ -10,5 +10,7 @@ public class MainActivityJava extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(() -> System.out.println("Runnable1.run()")).start();
+        new Thread(() -> System.out.println("Runnable2.run()")).start();
     }
 }
